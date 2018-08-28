11ffbfdf353d78c9e4973d261aeb9819cf78f21e
Paul Durbin <hello@pauldurbin.co.uk>
2018-08-28 15:00:52 +0100

    Update date_select_input.rb

--- lib/formtastic-bootstrap/inputs/date_select_input.rb
+++ lib/formtastic-bootstrap/inputs/date_select_input.rb
@@ -2,9 +2,9 @@ module FormtasticBootstrap
   module Inputs
     class DateSelectInput < Formtastic::Inputs::DateSelectInput
       FRAGMENT_CLASSES = {
-        :year   => "col-xs-4",
-        :month  => "col-xs-5",
-        :day    => "col-xs-3"
+        :year   => "col-4",
+        :month  => "col-5",
+        :day    => "col-3"
       }
 
       include Base
